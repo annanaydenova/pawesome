e87ab1a95aeb0e0584c27d3ce7d396eac679dd16
Richard Bates <richjbates@gmail.com>
2021-01-09 18:37:36 +0100

    added user story new animal

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,18 @@
 class AnimalsController < ApplicationController
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    @animal.save
+
+    redirect_to animals_path
+  end
+
+  private
+
+  def animals_params
+    params.require(:animal).permit(:name, :description, :animal_type, :photo, :address, :start_date, :end_date)
+  end
 end
