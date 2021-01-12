adf00f11a3fe7ca5c0514135a745b5dba09b9eb1
Richard Bates <richjbates@gmail.com>
2021-01-12 21:00:00 +0100

    added cloudinary

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,5 +2,6 @@ class Animal < ApplicationRecord
   has_many :bookings, dependent: :destroy
   belongs_to :user
   validates :name, presence: true, uniqueness: true
+  has_one_attached :photo
   # mount_uploader :photo, PhotoUploader
 end
