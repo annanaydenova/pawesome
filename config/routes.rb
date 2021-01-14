e018c9637fa6bb23ca8775ed46f31d705873f65b
Felix de Perthuis <fdeperthuis@sparkling-partners.com>
2021-01-14 21:07:37 +0100

    dashboard view

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: 'animals#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :animals
+  get 'dashboard', to: 'dashboards#index'
+  get 'dashboard/animals', to: 'dashboards#animals'
+  get 'dashboard/bookings', to: 'dashboards#bookings'
 end
