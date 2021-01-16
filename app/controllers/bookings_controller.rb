8fadd9229d3476b1941d6c40756fb8cfa9108b96
Richard Bates <richjbates@gmail.com>
2021-01-16 12:49:30 +0100

    booking routes, controllers, and form created

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,24 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @animal = Animal.find(params[:animal_id])
+  end
+
+  def create
+    @animal = Animal.find(params[:animal_id])
+    @start_date = params[:booking][:start_date]
+    @end_date = params[:booking][:end_date]
+    @booking = Booking.new(animal: @animal, start_date: @start_date, end_date: @end_date, user_id: current_user.id)
+    if @booking.save!
+      redirect_to animal_path(@animal)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:status, :start_date, :end_date)
+  end
 end
