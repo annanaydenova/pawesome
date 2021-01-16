b6b08491afc25d5f9964d54ce004559b396adef3
Anna Naydenova <anna.naydenova@gmail.com>
2021-01-16 12:07:45 +0100

    edit and destroy added to animals

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,12 +1,13 @@
 class AnimalsController < ApplicationController
   before_action :authenticate_user!, :except => [:index]
+  before_action :set_animal, only: [ :show, :edit, :update, :destroy ]
 
   def index
     @animals = Animal.all
   end
 
   def show
-    @animal = Animal.find(params[:id])
+    # @animal = Animal.find(params[:id])
     @booking = Booking.new
   end
 
@@ -22,7 +23,25 @@ class AnimalsController < ApplicationController
     redirect_to animals_path
   end
 
+  def edit
+  end
+
+  def update
+    @animal.update(animal_params)
+    redirect_to animal_path(@animal)
+  end
+
+  def destroy
+    # @animal = Animal.find(params[:id])
+    @animal.destroy
+    redirect_to animals_path
+  end
+
   private
+
+  def set_animal
+    @animal = Animal.find(params[:id])
+  end
 
   def animal_params
     params.require(:animal).permit(:name, :description, :animal_type, :photo, :address, :start_date, :end_date)
