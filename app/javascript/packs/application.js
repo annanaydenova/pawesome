e098b7c11c58c1e164345d334b4dd5e350790e7c
Richard Bates <richjbates@gmail.com>
2021-01-16 15:29:56 +0100

    front end changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,10 +29,12 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import { datePickr } from '../plugins/flatpickr';
+import { loadDynamicBannerText } from '../components/banner';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initAutocomplete();
   datePickr();
+  loadDynamicBannerText();
 });
