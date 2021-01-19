8f8f689cb24e64d743c16fd6529f0f64dee911c4
Richard Bates <richjbates@gmail.com>
2021-01-19 21:33:53 +0100

    added map and modified show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_193139) do
+ActiveRecord::Schema.define(version: 2021_01_19_183538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2021_01_12_193139) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_animals_on_user_id"
   end
 
