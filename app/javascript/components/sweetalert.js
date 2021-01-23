f6f9b4d9c1010b7a12a4ecc36d4ce192c396ccfe
Richard Bates <richjbates@gmail.com>
2021-01-23 12:53:11 +0100

    new alert for booking

--- app/javascript/components/sweetalert.js
+++ app/javascript/components/sweetalert.js
@@ -2,11 +2,15 @@ const sweetAlertOnBooking = () => {
   const AlertBtn = document.getElementById('booking-button');
   if (AlertBtn) {
     AlertBtn.addEventListener('click', (event)=>{
-      const Redirect = '<a href="../dashboard/bookings" style="color: white">ok</a>'
+      const Redirect = '<a href="../dashboard/bookings" style="color: white"><i class="fas fa-paw"></i> Great</a>'
       Swal.fire({
         title: "Booking pending!",
         text: "We have sent your booking request to the owner. Check the booking tab in your dashboard to see if your request has been accepted",
         icon: "success",
+        imageUrl: "https://media.giphy.com/media/l0unkiodQqmA3lPO5e/giphy.gif",
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
         confirmButtonText: Redirect
       });
     })
